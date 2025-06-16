02d753e1bd2b574dcb79c97d565a97f1e154c8aa
Victor Asiya <vicasasiya@gmail.com>
2025-06-16 02:09:32 +0100

    fixing CORS issues

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
-// ORIGINAL
-// require('dotenv').config();
-// const express = require('express');
-// const cors = require('cors');
-// const connectDB = require('./config/db');
-// const baseRouter = require('./routes');
-// const morgan = require('morgan');
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-// app.use(morgan("dev"))
-
-// // Connect DB
-// connectDB();
-
-// app.get("/", (req,res) => {
-//     res.json("Welcome to movie app web service...")
-// })
-
-// // Routes
-// app.use("/api",baseRouter);
-
-// module.exports = app;
-
-
-
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
